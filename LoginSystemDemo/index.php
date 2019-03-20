d993e608be96e7481a7789d6fb851fd4499fa873
JJ Gillian <45513258+jjgillian@users.noreply.github.com>
2019-03-20 19:35:55 -0400

    Update index.php

--- LoginSystemDemo/index.php
+++ LoginSystemDemo/index.php
@@ -1,7 +1,7 @@
 
 <?php require_once('header.php'); ?>
     
-    <h4 class="display-2 text-center mt-5">MATCHISURU</h4>
+    <h4 class="display-2 text-center mt-5" style="margin-top:130px !important">MATCHISURU</h4>
 
 <?php require_once('footer.php'); ?>    
 
